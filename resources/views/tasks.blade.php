2b6e6dcc7a583651eced52419d6cbc9c24e67277
Stanislav Polushin <stanisap85@gmail.com>
2020-06-30 14:57:17 +0300

    This application ended

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,10 +1,10 @@
 @extends('layout.app')
 
 @section('content')
-{{--        end form-horizontal--}}
+    {{--        end form-horizontal--}}
     <div class="card-body">
         @include('errors')
-        <form action="{{ url('tasks') }}" method="POST" class="form-horizontal">
+        <form action="{{ url('task') }}" method="POST" class="form-horizontal">
             {{ csrf_field() }}
 
             <div class="row">
@@ -27,7 +27,7 @@
                     <textarea type="text" name="description" id="task-description" class="form-control"></textarea>
                 </div>
             </div>
-            <div class="row">
+            <div class="row mb-2">
                 <div class="col-sm-3 mt-2">
                     <button type="submit" class="btn btn-success">
                         <i class="fa fa-plus">Add task</i>
@@ -35,8 +35,8 @@
                 </div>
             </div>
         </form>
-{{--        end form-horizontal--}}
-        @if(count($tasks) > 0)
+        {{--        end form-horizontal--}}
+        @if(isset($tasks) && count($tasks) > 0)
             <div class="card">
                 <div class="card-heading">
                     Current cards
@@ -45,15 +45,34 @@
                     <table class="table table-striped task-table">
                         <thead>
                         <th>Task</th>
+                        <th>Description</th>
                         <th>&nbsp;</th>
                         </thead>
                         <tbody>
                         @foreach($tasks as $task)
-                            
+                            <tr>
+                                <td class="table-text">
+                                    <div>{{ $task->title }}</div>
+                                </td>
+                                <td class="table-text">
+                                    <div>{{ $task->description }}</div>
+                                </td>
+                                <td>
+                                    <form action="{{ url('task/' . $task->id) }}" method="POST">
+                                        {{ csrf_field() }}
+                                        {{ method_field("DELETE") }}
+
+                                        <button class="btn btn-danger">
+                                            Delete
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
                         @endforeach
                         </tbody>
                     </table>
                 </div>
             </div>
+        @endif
     </div>
 @endsection
